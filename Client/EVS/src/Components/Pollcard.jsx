453374cc979a9499e3577d4f7bf796afae5ae122
Afonso Aguilar <afonso.aguilar12@gmail.com>
2026-05-16 14:46:32 +0100

    pollcards

---
+++ Client/EVS/src/Components/Pollcard.jsx
@@ -0,0 +1,15 @@
+function Pollcard(){
+    return(
+        <div className="bg-white w-25 m-5 h-100 rounded ">
+            <div>
+                <h1>TITLE</h1>
+            </div>
+            <div>
+                <button>PARTICIPATE</button>
+            </div>
+        </div>
+    )
+
+}
+
+export default Pollcard
--- Client/EVS/src/Pages/PollsPage.jsx
+++ Client/EVS/src/Pages/PollsPage.jsx
@@ -1,10 +1,19 @@
 import Navbar from "../Components/Navbar"
+import Pollcard from "../Components/Pollcard"
 
 function PollsPage({setView, isLoggedIn, onlogout}){
     return(
-        <div>
+        <section className="d-flex flex-column min-vh-100">
             <Navbar setView={setView} isLoggedIn={isLoggedIn} onlogout={onlogout}/>
-        </div>
+            <div className="container-fluid bg-dark flex-grow-1">
+                <div className="row">
+                    <div className="col">
+                        <Pollcard></Pollcard>
+
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
